69d2131746f42fcda2f38d2236afa59772d758c5
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 21:21:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  min_val = Float::Infinity
+  min_val = Float::INFINITY
   min_key
   hash.each do |k, v|
     if min_val > v
