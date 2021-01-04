e9a8abab783c1d3acc455b485e957d0d4bd73eb3
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 21:22:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,6 +13,6 @@ def key_for_min_value(hash)
   return min_key
 end
 
-veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+veggies = {:apple => -45, :banana => -44.5, :carrot => -44.9}
 
 puts key_for_min_value(veggies)
